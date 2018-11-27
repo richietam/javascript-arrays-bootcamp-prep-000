61004a477642ff211db527568006c76eec0ab65e
richardtam.ny <richardtam.ny@gmail.com>
2018-11-27 17:39:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,9 @@ function addElementToBeginningOfArray() {
   var array1 = ["foo", 1]
   return array1.unshift("object0")
 }
+
+function removeElementFromBeginningOfArray() {
+  var removeElementFromBeginningOfArray = [1, 2, 3]
+  removeElementFromBeginningOfArray = removeElementFromBeginningOfArray.slice(1)
+  removeElementFromBeginningOfArray
+}
