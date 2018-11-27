7295157545ebcbace8f73412b74aaf6ec87839ef
richardtam.ny <richardtam.ny@gmail.com>
2018-11-27 17:34:05 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray() {
+  var array1 = ["foo", 1]
+  return array1.unshift("object0")
+}
